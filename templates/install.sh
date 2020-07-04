4d2c6ecaba4494d821d089be3b97654bfba105d1
Eric Fischer <eric@eurocom-x7c.lan>
2020-07-04 15:02:52 +0200

    ok

--- templates/install.sh
+++ templates/install.sh
@@ -1,8 +1,8 @@
 mkdir -p /tmp/vbox;
-mount -o loop /tmp/VBoxGuestAdditions_5.2.34.iso /tmp/vbox;
+mount -o loop /tmp/VBoxGuestAdditions_{{ .product.version }}.iso /tmp/vbox;
 sh /tmp/vbox/VBoxLinuxAdditions.run \
     || echo "VBoxLinuxAdditions.run exited $? and is suppressed." \
         "createOrGet more read https://www.virtualbox.org/ticket/12479";
 umount /tmp/vbox;
 rm -rf /tmp/vbox;
-rm /tmp/VBoxGuestAdditions_5.2.34.iso;
+rm /tmp/VBoxGuestAdditions_{{ .product.version }}.iso;
