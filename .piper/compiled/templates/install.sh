2e04a197373e35d307826684d0ea8b8d46d1601e
Eric Fischer <eric@eurocom-x7c.lan>
2020-09-06 15:34:13 +0200

    ok

--- .piper/compiled/templates/install.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p /tmp/vbox;
-mount -o loop /tmp/VBoxGuestAdditions_{{ .product.version }}.iso /tmp/vbox;
-sh /tmp/vbox/VBoxLinuxAdditions.run \
-    || echo "VBoxLinuxAdditions.run exited $? and is suppressed." \
-        "createOrGet more read https://www.virtualbox.org/ticket/12479";
-umount /tmp/vbox;
-rm -rf /tmp/vbox;
-rm /tmp/VBoxGuestAdditions_{{ .product.version }}.iso;
